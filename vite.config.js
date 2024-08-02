0b3c963ed023fe5d59d67ff251e07235a1666ca0
Zilian Rifaldo <zilianrifaldof@gmail.com>
2024-08-02 14:20:10 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/deploy_react_app_github_pages_vercel",
+  base: process.env.VITE_BASE_PATH || "/deploy_tictactoe_github_pages_vercel",
+  build: {
+    outDir: "dist",
+  },
 });
